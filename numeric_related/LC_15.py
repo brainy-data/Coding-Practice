4e1ad3f3cda9e5ed21a1f439ec1611c3e301c03d
Brian, Mak Chung Yin <70089031+brainy-data@users.noreply.github.com>
2020-12-10 15:35:00 +0800

    Add files via upload

---
+++ numeric_related/LC_15.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 14:50:54 2020
+
+@author: Brian
+"""
+
+#剑指 Offer 15. 二进制中1的个数
+#JZoffer 15. count number of '1's in binary system
+#Example
+#Input:  11 
+#Output: 3
+#Note: 11 is [1011] in binary system
+
+n=11
+#Method 1 - strighforward one
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        #convert into binary number and count number of '1's
+        return  bin(n).count("1") 
+
+#Testing part
+solution=Solution()
+output=solution.hammingWeight(n)
+print(output)
+
+#Other methods - realted to bitwise operation (位運算)
+#To perform count operations without using count function
+#To study later**
+
+class Solution(object):
+    def hammingWeight(self, n):
+        res = 0
+        while n:
+            res += n & 1
+            n >>= 1
+        return res
+
+solution=Solution()
+output=solution.hammingWeight(n)
+print(output)
+
+class Solution(object):
+    def hammingWeight(self, n):
+        res = 0
+        while n:
+            res += 1
+            n &= n - 1
+        return res
+    
+solution=Solution()
+output=solution.hammingWeight(n)
+print(output)
